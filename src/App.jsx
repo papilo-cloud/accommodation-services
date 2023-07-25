ed51140fb4da5e06070511f1a485e917c5ccff1a
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-07-24 19:47:33 -0700

    booking comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+import MainBody from './components/MainBody'
+import Room from './components/Room'
+import Booking from './components/Booking'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-   
-    </>
+    <div className="app">
+      {/* <Header />
+      <MainBody /> */}
+      {/* <Room /> */}
+      <Booking />
+    </div>
   )
 }
 
--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import data from '../data.js'
+import './mainbody.css'
+import image from '../assets/images/images1.jpeg'
+
+console.log(data)
+
 
 const MainBody = () => {
   return (
-    <div>MainBody</div>
+    <div className='main-body'>
+      <div className="main">
+        <h1>Find</h1>
+        <p className='p'>Your best accomodation.</p>
+        {/* {data.map(item => <div className="data">Hello</div>)} */}
+        <div className="data">
+          <div className="img">
+            <img src={image} alt="image" />
+          </div>
+          <div className="text">
+            <p><span className='avail'>available</span> <span className='price'>$300.00</span></p>
+            <p>
+              <span className="name">Deluxe Suites.</span> <span className='like'></span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
